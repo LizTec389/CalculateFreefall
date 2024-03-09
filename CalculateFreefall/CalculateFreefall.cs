653a75de307f0ddb99deada26bf7f3e6abbfb8f1
LizethCarriQui <LizethCarriQui@LIIZETHQUICARRI>
2024-03-09 00:04:35 -0600

    Agregar archivos de proyecto.

---
+++ CalculateFreefall/CalculateFreefall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateFreefall
+{
+    public class CalculateFreefall
+    {
+        public static double CalculateVelocityFinal(double height)
+        {
+            const double gravity = 9.8;/*  Acceleration due to gravity in m / s²*/
+            return Math.Sqrt(2 * gravity * height);
+        }
+
+
+    }
+}
---
+++ CalculateFreefall/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+        Console.Write("Enter the height (in meters): ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        double speedFinal = CalculateFreefall.CalculateVelocityFinal(height);
+
+        Console.WriteLine($"Final velocity is: {speedFinal} m/s");
+    }
+    
+   }
